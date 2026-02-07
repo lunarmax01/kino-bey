e8e698160e2a1b3ec4829335954224c5a6f2f47a
BEY <lunargold777@gmail.com>
2026-02-07 20:42:18 +0500

    full

--- index.js
+++ index.js
@@ -418,7 +418,7 @@ async function handleAdminInput(chatId, msg) {
 
                 const finalData = { code, ...st.data, addedBy: chatId, contentType: st.contentType };
                 if (st.contentType === 'series') {
-                    finalData.episodes = [{ number: 1, fileId: st.data.fileId, name: '1' }];
+                    finalData.episodes = [{ number: 1, fileId: st.data.fileId, name: '1-qism' }];
                     delete finalData.fileId;
                 }
                 const movie = await Movie.create(finalData);
@@ -897,7 +897,7 @@ function generateSeriesKeyboard(movie, page = 0) {
     const currentEps = movie.episodes.slice(start, end);
 
     const epButtons = currentEps.map(ep => ({
-        text: `${ep.number}-qism`,
+        text: `${ep.number}`,
         callback_data: `s_dl_${movie.code}_${ep.number}`
     }));
 
